b5ccc303c84472f92c417bb35c0a12f665dd66be
maksim.kan <maksim.kan@rtlabs.ru>
2023-10-27 23:20:43 +0300

    subjects selection, math calc working

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import telebot
+import re
+
+subject='select'
 
 API_TOKEN = '6048445093:AAGZIK0JIPJHzdlELYBQw2MzjJUBW5OA2ms'
 
@@ -8,16 +11,37 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
+    global subject
+    subject='select'
     bot.reply_to(message, """
-kupi moi slon
+    1-матеша
+    2-русский
+    3-инглиш
 """)
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    reply='vse govoryat '+ message.text+' kupi slon a to arabam sdam'
+    global subject
+    text= message.text
+    if subject=='select':
+        if text=='1':
+            reply='напиши пример,я решу "1024-(356*6/3+1)"'
+            subject = 'math'
+        elif text=='2':
+            reply='nu sory v ananas idite'
+            subject = 'rus'
+        elif text=='3':
+            reply='напиши текст на русском,я перведу'
+            subject = 'eng'
+    elif subject=='math':
+        pattern ='^[0-9+-/\*\(\) ]*$'
+        if re.match(pattern, text):
+            reply = eval(text)
+        else:
+            reply = 'ERROR 404'
+   # reply='vse govoryat '+ message.text+' kupi slon a to arabam sdam'
     bot.reply_to(message,reply )
-
 
 bot.infinity_polling()
